6e3e96061452fade444f87600fa12e20fe7e6d97
rajkumar patel <patelrajkumar362001@gmail.com>
2020-09-08 20:31:44 +0530

    Removing not Usefull Comments from index.js file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
-    // var cors = require('cors')
 const app = express()
-    // app.use(cors())
 const publicPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../public/views')
 const partialPath = path.join(__dirname, "../public/partials")
 hbs.registerPartials(partialPath)
 app.use(express.static(publicPath))
 app.set('views', viewsPath)
-    // var corsOptions = {
-    //         origin: '*',
-    //         optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
-    //     }
-    // app.use(cors(corsOptions));
-
-
 app.get('', (req, res) => {
-    res.render('index.hbs', { title: 'rajkumar' })
+    res.render('index.hbs')
 })
 app.listen(8000, () => console.log("server is running......"));
